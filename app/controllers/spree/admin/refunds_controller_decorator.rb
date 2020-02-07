af307f58fca601369d755fd60c7025ba55ce988a
judyn <judy.njagi@gmail.com>
2020-02-08 02:11:52 +0300

    Replace class eval with module prepend

--- app/controllers/spree/admin/refunds_controller_decorator.rb
+++ app/controllers/spree/admin/refunds_controller_decorator.rb
@@ -1,9 +1,16 @@
-Spree::Admin::RefundsController.class_eval do
-  after_action :check_error_flashes, only: :create
-
-  private
-    def check_error_flashes
-      error_msg = @object.errors.full_messages.join('. ')
-      flash[:error] = error_msg if error_msg.present?
+module Spree
+  module Admin
+    module RefundsControllerDecorator
+      def self.prepended(base)
+        base.after_action :check_error_flashes, only: :create
+      end
+      private
+        def check_error_flashes
+          error_msg = @object.errors.full_messages.join('. ')
+          flash[:error] = error_msg if error_msg.present?
+        end
     end
+  end
 end
+
+::Spree::Admin::RefundsController.prepend(Spree::Admin::RefundsControllerDecorator)
